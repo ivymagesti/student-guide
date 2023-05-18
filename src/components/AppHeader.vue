381e56a6cdc68449f8b5c6f8193350a0350bbbbc
Ivy Magesti <oliveiraivy67@gmail.com>
2023-05-18 16:19:11 -0300

    feat: initializing project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <nav>
+            <a href="/"><img id="logo" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg">
+        </a>
+        <img alt="abrir menu" id="menu-button" src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg">
+        </nav>
+    </header>
+</template>
+
+<script>
+export default{
+    name: 'AppHeader',
+}
+</script>
+
+<style scoped>
+
+header{
+    background-color : var(--color-background-nav);
+    height: 60px;
+    width: 100%;
+}
+#logo{
+    width: 130px;
+}
+#menu-button{
+    width: 30px;
+}
+
+</style>
